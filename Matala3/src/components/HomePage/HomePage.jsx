a7cdfab1fb6c47891267153d45d5228884bd19c3
hilatsivion <hilatsivion2@gmail.com>
2024-12-20 15:25:17 +0200

    create a navbar

--- Matala3/src/components/HomePage/HomePage.jsx
+++ Matala3/src/components/HomePage/HomePage.jsx
@@ -1,27 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./style.css";
 import "../../general.css";
-import logo from "../../../assets/images/logo.png";
+import Navbar from "../Navbar/Navbar.jsx";
 import bubbles from "../../../assets/images/bubbles-big.png";
 
 function HomePage() {
   return (
     <div className="home-container">
-      <header className="home-header">
-        <div className="logo">
-          <img src={logo} alt="logo" />
-          <h1>Matala3</h1>
-        </div>
-        <div className="buttons">
-          <button className="btn create-account">
-            <Link to="/register-page">Create Account</Link>
-          </button>
-          <button className="btn login">
-            <Link to="/login-page">Log in</Link>
-          </button>
-        </div>
-      </header>
+      <Navbar />
       <main className="home-main">
         <div className="welcome-text">
           <h1 className="title-header">Welcome to Matala 3</h1>
---
+++ Matala3/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/images/logo.png";
+import "./style.css";
+
+function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <header className="navbar">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+        <h1>Matala3</h1>
+      </div>
+      <div className="buttons">
+        {!isLoggedIn ? (
+          <>
+            <button className="btn create-account">
+              <Link to="/register-page">Create Account</Link>
+            </button>
+            <button className="btn login">
+              <Link to="/login-page">Log in</Link>
+            </button>
+          </>
+        ) : (
+          <div className="profile">
+            <div className="profile">
+              <img
+                src="/path/to/profile-image.jpg" // Replace with the actual image path
+                alt="Profile"
+                className="profile-image"
+              />
+              <h4 className="profile-text">Profile</h4>
+            </div>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
